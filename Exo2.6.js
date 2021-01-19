7806c42f8bb8b33c435c027223e3bb20614dd960
Naudar79 <naudar79@gmail.com>
2021-01-19 15:34:51 +0100

    add file
    Exo2.6

---
+++ Exo2.6.js
@@ -0,0 +1,19 @@
+const readlineSync = require("readline-sync");
+let num = new Number(readlineSync.question('What is your favorite number ?'));
+if ((num == 1) || (num <= 7)) {
+    if (num == 1) {
+        console.log("Monday");
+    } else if (num == 2) {
+        console.log("Tuesday");
+    } else if (num == 3) {
+        console.log("Wednesday");
+    } else if (num == 4) {
+        console.log("Thursday");
+    } else if (num == 5) {
+        console.log("Friday");
+    } else if (num == 6) {
+        console.log("Saturday");
+    } else {
+        console.log("Sunday");
+    }
+}
