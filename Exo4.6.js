0f04fbe5a2efaecd97dbbd9e3bb8d7d46796b883
Naudar79 <naudar79@gmail.com>
2021-01-20 15:12:25 +0100

    maj Exo4.6
    update

--- Exo4.6.js
+++ Exo4.6.js
@@ -1,10 +1,10 @@
 const readlineSync = require("readline-sync");
 let num = new Number(readlineSync.question('Enter a number : '));
 
-function factorial(num) {
-    let result = 1;
-    for (let i = 1; i <= num; i++)
-        result = result * i;
-    return result;
+function factorial(nb) {
+    if (nb == 0) {
+        return 1;
+    }
+    return nb * factorial(nb - 1);
 }
 console.log(factorial(num));
