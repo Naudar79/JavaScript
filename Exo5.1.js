f26edd02533f5598c6b3da806955701ae66de804
Naudar79 <naudar79@gmail.com>
2021-01-21 08:20:19 +0100

    Update Exo5.1.js

--- Exo5.1.js
+++ Exo5.1.js
@@ -5,7 +5,7 @@ let choix = '';
 function askTvSeries() {
     sondage.name = readlineSync.question('Entrez le nom de votre serie :');
     sondage.year = new Number(readlineSync.question('Entrez la date de votre serie :'));
-    //do {
+    //do {              !!!! Finir la boucle + alimenter le tableau
     sondage.cast = readlineSync.question('Entrez le nom de votre acteur. faites Q pour arrêter la liste :');
     //} while ((choix != "q") || (choix != "Q"));
     return sondage;
